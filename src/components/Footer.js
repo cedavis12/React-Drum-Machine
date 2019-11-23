770b99de8a2cb0102b7004f573f0ca07390f7326
Courtney Davis <courtneydavis@The-Davis-iMac.local>
2019-11-23 14:07:50 -0500

    add header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <nav className='navbar fixed-bottom navbar-dark bg-dark'>
+      <span className='navbar-brand mb-0 h1'>
+        Made with &hearts; by Courtney Davis
+      </span>
+    </nav>
+  );
+}
+
+export default Footer;
