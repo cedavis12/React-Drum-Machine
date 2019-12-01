923c4650fa58a06757aa5d9eecc3a7bf3d1bd8f2
Courtney Davis <courtneydavis@The-Davis-iMac.local>
2019-11-30 20:49:36 -0500

    Add DrumMachine component

---
+++ src/components/DrumMachine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DrumMachine(props) {
+  return (
+    <div id='drum-machine'>
+      <div id='display'></div>
+    </div>
+  );
+}
+
+export default DrumMachine;
